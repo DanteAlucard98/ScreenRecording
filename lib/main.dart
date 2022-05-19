f178c7f5416d739e7ce7640ec5eb6fca70e186da
DanteAlucard98 <dantealucard98.dp@gmail.com>
2022-05-19 12:47:43 -0500

    example screen recording

---
+++ lib/main.dart
@@ -0,0 +1,118 @@
+import 'dart:developer';
+import 'dart:io';
+import 'package:ed_screen_recorder/ed_screen_recorder.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter VideoRecord',
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+      ),
+      home: MyHomePage(title: 'Flutter VideoRecord'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  EdScreenRecorder? screenRecorder;
+  Map<String, dynamic>? _response;
+  bool inProgress = false;
+
+  @override
+  void initState() {
+    super.initState();
+    screenRecorder = EdScreenRecorder();
+  }
+
+  Future<void> startRecord({required String fileName}) async {
+    try {
+      var startResponse = await screenRecorder?.startRecordScreen(
+        fileName: "Eren",
+        audioEnable: false,
+      );
+      setState(() {
+        _response = startResponse;
+      });
+      try {
+        screenRecorder?.watcher?.events.listen(
+          (event) {
+            log(event.type.toString(), name: "Event: ");
+          },
+          onError: (e) => kDebugMode ? debugPrint('ERROR ON STREAM: $e') : null,
+          onDone: () => kDebugMode ? debugPrint('Watcher closed!') : null,
+        );
+      } catch (e) {
+        kDebugMode ? debugPrint('ERROR WAITING FOR READY: $e') : null;
+      }
+    } on PlatformException {
+      kDebugMode
+          ? debugPrint("Error: An error occurred while starting the recording!")
+          : null;
+    }
+  }
+
+  Future<void> stopRecord() async {
+    try {
+      var stopResponse = await screenRecorder?.stopRecord();
+      setState(() {
+        _response = stopResponse;
+      });
+    } on PlatformException {
+      kDebugMode
+          ? debugPrint("Error: An error occurred while stopping recording.")
+          : null;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Screen Recording Debug"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("File: ${(_response?['file'] as File?)?.path}"),
+            Text("Status: ${(_response?['success']).toString()}"),
+            Text("Event: ${_response?['eventname']}"),
+            Text("Progress: ${(_response?['progressing']).toString()}"),
+            Text("Message: ${_response?['message']}"),
+            Text("Video Hash: ${_response?['videohash']}"),
+            Text("Start Date: ${(_response?['startdate']).toString()}"),
+            Text("End Date: ${(_response?['enddate']).toString()}"),
+            ElevatedButton(
+                onPressed: () => startRecord(fileName: "eren"),
+                child: const Text('START RECORD')),
+            ElevatedButton(
+                onPressed: () => stopRecord(),
+                child: const Text('STOP RECORD')),
+          ],
+        ),
+      ),
+    );
+  }
+}
